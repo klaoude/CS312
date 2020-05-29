885bb353192c0b2050c4868d1187106e8cc74b61
klaoude <neoklaoude@gmail.com>
2020-05-29 16:02:10 +0200

    add args

--- TP-Eval/src/TestFSMIOViewer.java
+++ TP-Eval/src/TestFSMIOViewer.java
@@ -1,7 +1,18 @@
 public class TestFSMIOViewer {
 
 	public static void main(String argv[]) {
-		new TestFSMIO();
-		new Test();
+		if(argv.length != 1)
+		{
+			System.out.println("Input args !\n\t1 = Exo1\n\t2 = Exo2\n\t3 = Exo3\n\ts = test serialisation");
+			System.exit(0);
+		}
+		if(argv[0].equals("s"))
+			new TestFSMIO();
+		else if(argv[0].equals("3"))
+			new Test();
+		else if(argv[0].equals("1"))
+			new FSMIOStringviewer();
+		else if(argv[0].equals("2"))
+			new FSMIOViewer();
 	}
 }
